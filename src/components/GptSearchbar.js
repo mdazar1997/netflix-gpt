8cb2d8c4e3ceebc0218d6f9caaef7b811ae064e0
mohamedazar <mohamedazar106@gmail.com>
2024-04-25 11:11:17 +0400

    unused variable fix

--- src/components/GptSearchbar.js
+++ src/components/GptSearchbar.js
@@ -1,7 +1,7 @@
 import React,{useRef} from 'react'
 import lang from '../utils/languageConstants';
 import { useSelector ,useDispatch} from 'react-redux';
-import { openai } from '../utils/openai';
+// import { openai } from '../utils/openai';
 import { API_OPTIONS } from '../utils/constants';
 import { addGptMovieResult } from '../utils/gptSlice';
 
