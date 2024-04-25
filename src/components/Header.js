4445fa0fd042e307ffbd0c924a786bd946b2077e
mohamedazar <mohamedazar106@gmail.com>
2024-04-25 11:17:34 +0400

    useeffect fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
       }
     });
     return ()=>unsubscribe()
-  },[dispatch])
+  },[dispatch,navigate])
   return (
     <div className='absolute px-8 py-2 bg-gradient-to-b from-black w-screen z-10 flex flex-col md:flex-row justify-between'> 
         <img className='w-44 mx-auto md:mx-0' src={LOGO}
